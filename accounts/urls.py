7e86ba131f13ac3c75a0b6508bd23a02b7237b63
woodzverse <woodzverse@naver.com>
2024-11-10 18:52:24 +0900

    accounts 파일 urls.py 주석 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,9 +5,9 @@ from .views import CustomRegisterView, CustomLoginView, CustomLogoutView, delete
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', CustomRegisterView.as_view(), name='signup'),
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('delete/', delete_user, name='delete_user'),
-    path('info/', create_user_info, name='create_user_info'),
+    path('signup/', CustomRegisterView.as_view(), name='signup'), # 회원가입
+    path('login/', CustomLoginView.as_view(), name='login'), # 로그인
+    path('logout/', CustomLogoutView.as_view(), name='logout'), # 로그아웃
+    path('delete/', delete_user, name='delete_user'), # 회원탈퇴
+    path('info/', create_user_info, name='create_user_info'), # 유저 정보 생성
 ]
