81fc61c1591036e354980854bea2dd72d1e017fa
HeheHeewon <qtrachel@naver.com>
2024-11-23 02:35:30 +0900

    정적 파일 내용 수정

--- ganzithon/settings.py
+++ ganzithon/settings.py
@@ -165,7 +165,6 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
